e836940042b9282e7fac9df846564ea50ccc36f9
Spencer Pierce <spencer82188@gmail.com>
2019-04-21 18:44:59 -0700

    Completed the 6ees6ees6ees to pass all tests correctly I think

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor () {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage (treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
+// a canFly property that is set true
+// a treasureChest property that is set to an empty array []
+// a forage method that allows the bee to add a treasure to the treasureChest
